abdf46e30af37453c50b1e64114561e7228ef811
Eric Van Dusen <ericvd@gmail.com>
2020-08-19 17:25:40 -0700

    week 1

---
+++ Fall_2020/week_01/assignment/tests/.ipynb_checkpoints/q1_1-checkpoint.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 31536000.0 < sec_per_year < 31557600.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Fall_2020/week_01/assignment/tests/q1_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 2.5,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> -90.0 <= birthquake_latitude <= 90.0 && -180.0 <= birthquake_longitude <= 180.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}

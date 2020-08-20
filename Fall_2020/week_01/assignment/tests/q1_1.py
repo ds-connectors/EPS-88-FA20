a5eb71ff840357ab4bf3a56ea080a6e1a9eb844b
Eric Van Dusen <ericvd@gmail.com>
2020-08-19 17:21:11 -0700

    Add files via upload

---
+++ Fall_2020/week_01/assignment/tests/q1_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 2.5,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 37.0 <= Berkeley_latitude <= 38.0 && -123.0 <= Berkeley_longitude <= -122.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}

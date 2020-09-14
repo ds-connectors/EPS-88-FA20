580194ca74dd1faa68557ef1a62eaeb53021d4b1
Maggie Avery <msavery@berkeley.edu>
2020-09-13 18:46:00 -0700

    week 3 materials

---
+++ week_03/assignment/tests/q1_2.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> earthquakes_mag5.describe().loc['count']['latitude'] == 16250.0
+          True
+          >>> earthquakes_mag6.describe().loc['count']['latitude'] == 1297.0
+          True
+          >>> earthquakes_mag7.describe().loc['count']['latitude'] == 137.0
+          True
+          >>> earthquakes_mag8.describe().loc['count']['latitude'] == 9.0
+          True
+          >>> earthquakes_mag9.describe().loc['count']['latitude'] == 1.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ week_03/assignment/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 3333.0 < dist < 3450.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}

bccf6eebd0c90c20d0677a1e5b97cf4ef0fdf019
Maggie Avery <msavery@berkeley.edu>
2020-09-15 12:22:23 -0700

    fix

---
+++ week_03/inclass/tests/q1_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> EQ_data.describe().loc['max']['mag'] == 9.1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
